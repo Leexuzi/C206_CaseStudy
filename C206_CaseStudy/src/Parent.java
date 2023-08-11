be5e652f0862181aa0a151ea4b22ed466a69d5f5
leezixu <22027176@myrp.edu.sg>
2023-08-11 14:43:08 +0800

    changed parent

--- C206_CaseStudy/src/Parent.java
+++ C206_CaseStudy/src/Parent.java
@@ -18,32 +18,10 @@
 
 public class Parent extends User {
     private String paymentPreferred;
-    private List<String> dietaryPreferences;
-    private List<String> allergies;
-    private List<Order> orders;
 
     public Parent(String username, String password, String paymentPreferred) {
         super(username, password);
         this.paymentPreferred = paymentPreferred;
-        this.dietaryPreferences = new ArrayList<>();
-        this.allergies = new ArrayList<>();
-        this.orders = new ArrayList<>();
     }
 
-    public void placeOrder(Order order) {
-        orders.add(order);
-        // Add orders
-    }
-
-    public void trackOrders() {
-        // Track orders
-    }
-
-    public void addDietaryPreference(String preference) {
-        dietaryPreferences.add(preference);
-    }
-
-    public void addAllergy(String allergy) {
-        allergies.add(allergy);
-    }
 }
