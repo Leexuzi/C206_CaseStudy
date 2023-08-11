d774a309e5c71b42728b95308bc59d536aec389b
leezixu <22027176@myrp.edu.sg>
2023-08-11 12:28:55 +0800

    sd

---
+++ C206_CaseStudy/src/Admin.java
@@ -0,0 +1,11 @@
+
+public class Admin extends User{
+	
+	private String ASDFG;
+
+	public Admin(String username, String password) {
+		super(username, password);
+		// TODO Auto-generated constructor stub
+	}
+
+}
