81a03df6193f6099156a302a7ddac83bb1f2ddb1
Ashley <22000765@myrp.edu.sg>
2023-08-11 11:46:33 +0800

    Vendor.java

--- C206_CaseStudy/src/Parent.java
+++ C206_CaseStudy/src/Parent.java
@@ -1,41 +1,3 @@
-
-<<<<<<< HEAD
-public class Parent {
-	private String assetTag;
-	private String description;
-	private String dueDate;
-	private boolean isAvailable;
-
-	public Parent(String assetTag, String description) {
-		this.assetTag = assetTag;
-		this.description = description;
-		this.dueDate = "";
-		this.isAvailable = true;
-	}
-
-	public String getAssetTag() {
-		return assetTag;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public String getDueDate() {
-		return dueDate;
-	}
-
-	public void setDueDate(String dueDate) {
-		this.dueDate = dueDate;
-	}
-
-	public boolean getIsAvailable() {
-		return isAvailable;
-	}
-
-	public void setIsAvailable(boolean isAvailable) {
-		this.isAvailable = isAvailable;
-=======
 public class Parent extends User{
 	private String paymentPreferred;
 
@@ -50,7 +12,6 @@ public class Parent extends User{
 	
 	public void setPaymentMethod(String paymentPreferred) {
 		this.paymentPreferred = paymentPreferred;
->>>>>>> branch 'master' of https://github.com/Leexuzi/C206_CaseStudy.git
 	}
 }
 
