84e12396557dcde56bd11ff296f263e01d1ae39f
WayneKoh7 <131931486+WayneKoh7@users.noreply.github.com>
2023-08-11 15:17:49 +0800

    Update Vendor.java

--- C206_CaseStudy/src/Vendor.java
+++ C206_CaseStudy/src/Vendor.java
@@ -1,24 +1,11 @@
-<<<<<<< HEAD
-=======
-import java.util.ArrayList;
->>>>>>> branch 'master' of https://github.com/Leexuzi/C206_CaseStudy.git
-
 public class Vendor extends User {
     private String contactInfo;
     private String address;
-<<<<<<< HEAD
-    private List<Menu> menus;  // Corrected the type and initialization
-=======
->>>>>>> branch 'master' of https://github.com/Leexuzi/C206_CaseStudy.git
 
     public Vendor(String username, String password, String contactInfo, String address) {
         super(username, password);
         this.contactInfo = contactInfo;
         this.address = address;
-<<<<<<< HEAD
-        menus = new ArrayList<>();  // Corrected the initialization
-=======
->>>>>>> branch 'master' of https://github.com/Leexuzi/C206_CaseStudy.git
     }
 
     public String getContactInfo() {
@@ -36,13 +23,4 @@ public class Vendor extends User {
     public void setAddress(String address) {
         this.address = address;
     }
-<<<<<<< HEAD
-
-    @Override
-    public String toString() {
-        return super.getUsername() + "\nVendor: " + super.getUsername() +
-               "\nContact: " + contactInfo + "\nAddress: " + address;
-    }
-=======
->>>>>>> branch 'master' of https://github.com/Leexuzi/C206_CaseStudy.git
 }
