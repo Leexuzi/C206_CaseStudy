5bee59ed0d5904392114cece4fba4cfe75a86dba
Admin <Admin@10.175.3.51>
2023-08-10 21:09:12 -0700

    Changed school

--- C206_CaseStudy/src/School.java
+++ C206_CaseStudy/src/School.java
@@ -14,6 +14,29 @@
  *
  */
 public class School {
-
+    
+	private String name;
+	private int schoolID;
+	
+	public School(String name, int schoolID) {
+		this.name = name;
+		this.schoolID = schoolID;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getSchoolID() {
+		return schoolID;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setSchoolID(int schoolID) {
+		this.schoolID = schoolID;
+	}
 }//test
 
